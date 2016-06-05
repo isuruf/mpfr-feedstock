b018dffb1c280b1af974e6c95cca5d7c7e53ee76
Isuru Fernando <isuru.11@cse.mrt.ac.lk>
2016-06-05 13:08:19 +0530

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,24 +1,36 @@
-set mpfr_root=%cd%
-cd ..
-mkdir mpir
-xcopy %PREFIX%\mpir mpir\ /E
-
-cd %mpfr_root%\build.vc14
-
 if "%ARCH%"=="32" (
     set PLATFORM=Win32
 ) else (
     set PLATFORM=x64
 )
 
+set mpfr_root=%cd%
+cd ..
+mkdir mpir\dll\%PLATFORM%\Release
+mkdir mpir\lib\%PLATFORM%\Release
+
+REM copy mpir headers
+xcopy %LIBRARY_INC%\*.h mpir\lib\%PLATFORM%\Release\ /E
+xcopy %LIBRARY_INC%\*.h mpir\dll\%PLATFORM%\Release\ /E
+
+REM copy mpir libraries
+copy %LIBRARY_LIB%\mpir.dll mpir\dll\%PLATFORM%\Release\mpir.dll
+copy %LIBRARY_LIB%\mpir.lib mpir\dll\%PLATFORM%\Release\mpir.lib
+copy %LIBRARY_LIB%\mpir_static.lib mpir\lib\%PLATFORM%\Release\mpir.lib
+
+cd %mpfr_root%\build.vc14
+
 msbuild.exe /p:Platform=%PLATFORM% /p:Configuration=Release lib_mpfr\lib_mpfr.vcxproj
+msbuild.exe /p:Platform=%PLATFORM% /p:Configuration=Release dll_mpfr\dll_mpfr.vcxproj
 
 REM msbuild.exe /p:Platform=%PLATFORM% /p:Configuration=Release lib_mpfr_tests\lib_tests\lib_tests.vcxproj
 REM python %mpfr_root%\build.vc14\run_lib_tests.py
 
-mkdir %PREFIX%\mpfr\lib\%PLATFORM%\Release
-xcopy lib %PREFIX%\mpfr\lib\ /E
+copy dll\%PLATFORM%\Release\mpfr.lib %LIBRARY_LIB%\mpfr.lib
+copy dll\%PLATFORM%\Release\mpfr.dll %LIBRARY_BIN%\mpfr.dll
+copy dll\%PLATFORM%\Release\mpfr.pdb %LIBRARY_BIN%\mpfr.pdb
+
+copy lib\%PLATFORM%\Release\mpfr.lib %LIBRARY_BIN%\mpfr_static.lib
 
 cd ..
-xcopy lib %PREFIX%\mpfr\lib\ /E
-
+copy dll\%PLATFORM%\Release\mpfr.h %LIBRARY_INC%\mpfr.h
